c52b5f6ded559ef4d0bd51523863f874fbfacaab
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-05-04 07:30:43 +0700

    add NavLink component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
+import NavLink from "./navLink";
 const links = [
   { url:"/" , title:"Home"},
   { url:"/about" , title:"About"},
@@ -13,15 +14,15 @@ const links = [
 const Navbar = () => {
   const [open,setOpen] = useState(false);
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINK */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link)=>(
-          <Link key={link.url} href={link.url}>{link.title}</Link>
+          <NavLink key={link.url} link={link}/>
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
           <span className="text-white mr-1">Junie</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.dev</span>
