642836d408ccb63130bcc7ff2cdaaca1984abd14
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-05-06 18:15:13 +0700

    add project list and animations to the Portfolio page.

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,13 +1,75 @@
  "use client"
-import {motion} from "framer-motion"
+import {motion, useScroll, useTransform} from "framer-motion"
+import { useRef } from "react"
+import Image from "next/image";
+import Link from "next/link";
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://fastly.picsum.photos/id/20/3670/2462.jpg?hmac=CmQ0ln-k5ZqkdtLvVO23LjVAEabZQx2wOaT4pyeG10I",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://fastly.picsum.photos/id/24/4855/1803.jpg?hmac=ICVhP1pUXDLXaTkgwDJinSUS59UWalMxf4SOIWb9Ui4",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://fastly.picsum.photos/id/48/5000/3333.jpg?hmac=y3_1VDNbhii0vM_FN6wxMlvK27vFefflbUSH06z98so",
+    link: "https://lama.dev",
+  },
+];
 const PortfolioPage = () => {
-  return (
+
+  const ref = useRef()
+  const {scrollYProgress} = useScroll({target:ref})
+  const x = useTransform(scrollYProgress,[0,1],["0%","-80%"])
+   return (
     <motion.div
       className="h-full"
       initial={{y:"-200vh"}}
       animate={{y:"0%"}}
       transition={{duration:1}}
-    >PortfolioPage</motion.div>
+    >
+      <div className="h-[600vh]" ref={ref}> 
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">My Works</div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+          <motion.div style={{x}} className="flex">
+            <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300"/>
+            {items.map(item=>(
+              <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
+                <div className="flex flex-col gap-8 text-white">
+                  <h1>{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} alt="" fill></Image>
+                  </div>
+                  <p>{item.desc}</p>
+                  <Link href={item.link}><button>See Demo</button></Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
