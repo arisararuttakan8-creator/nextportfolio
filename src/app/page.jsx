c3245ed37dcacdebc7c7ec8f7ae572b49d46b1b5
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-05-16 11:27:01 +0700

    add img profile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,10 +10,10 @@ const Homepage = () => {
     animate={{y:"0%"}}
     transition={{duration:1}}
   >
-    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 gap-4">
       {/* IMAGE CONTAINNER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image src="/hero.png" alt="" fill className="object-contain"></Image>
+        <Image src="/profile8.png" alt="" fill className="object-contain"></Image>
       </div>
       {/* TEXT CONTAINNER */}
       <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
