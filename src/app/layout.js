3596892f99669e3a615b63c8c0a30c1c8643cb91
Arisara Ruttakan <arisararuttakan8@gmail.com>
2026-05-21 16:43:42 +0700

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import { GoogleAnalytics } from '@next/third-parties/google' 
 
 const inter = Inter({ subsets: ["latin"] });
 
